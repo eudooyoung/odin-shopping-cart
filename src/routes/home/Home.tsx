fb0aeb3939c6f63d7dfb949bd66d157c6bc4961d
Kim Doo Young <177651948+eudooyoung@users.noreply.github.com>
2026-04-15 18:59:33 +0900

    feat: fill home page with random words

--- src/routes/home/Home.tsx
+++ src/routes/home/Home.tsx
@@ -2,6 +2,31 @@ export default function Home() {
   return (
     <>
       <h2>Home Page</h2>
+      <p>
+        <i>We know you want it! Don't hesitate it. Just get what you desire!</i>
+      </p>
+      <section>
+        <h3>What we provide</h3>
+        <p>
+          Have you ever wonder why you can't get what you want? That's because
+          you didn't know us!
+          <br />
+          With Odin Shopping Cart, your{" "}
+          <strong>
+            <i>Dream</i>
+          </strong>{" "}
+          may come true!
+        </p>
+      </section>
+      <section>
+        <h3>About us</h3>
+        <p>
+          Congratulation on finally finding us! Once you found us, don't worry
+          more.
+          <br />I guarantee you that we are the best provider you will ever
+          meet.
+        </p>
+      </section>
     </>
   );
 }
